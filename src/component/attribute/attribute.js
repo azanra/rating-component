6741f433f849a310028303fcec03ac5778e2a49e
azan-personal <112457939+azanra@users.noreply.github.com>
2025-03-11 08:47:14 +0700

    Style and position header component

--- src/component/attribute/attribute.js
+++ src/component/attribute/attribute.js
@@ -1,7 +1,7 @@
 export const componentAttribute = {
   question: {
     header: {
-      text: "How did we do",
+      text: "How did we do?",
     },
     body: {
       text: "Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!",
--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,3 +1,5 @@
 export default function Header({ text }) {
-  return <h1>{text}</h1>;
+  return (
+    <h1 className="text-3xl text-white font-semibold mt-10 mb-5">{text}</h1>
+  );
 }
