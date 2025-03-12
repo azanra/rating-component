1e050fef606060d3d2479685f1e33554890a7e98
azan-personal <112457939+azanra@users.noreply.github.com>
2025-03-12 09:11:06 +0700

    Hide the radio and turn the label to inline block

--- src/component/checkbox.jsx
+++ src/component/checkbox.jsx
@@ -1,13 +1,16 @@
 export default function Checkbox({ value, handleSetRating }) {
   return (
     <>
-      <label htmlFor={`rating-${value}`}>{value}</label>
+      <label htmlFor={`rating-${value}`} className="inline-block">
+        {value}
+      </label>
       <input
         type="radio"
         id={`rating-${value}`}
         name="rating"
         value={value}
         onChange={handleSetRating}
+        className="hidden"
       />
     </>
   );
